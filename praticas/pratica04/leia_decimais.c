480612aea6b505df625adcc2cbf40c7dd700eae2
Leo.pultrini <leo.pultrini@outlook.com>
2026-03-25 20:12:01 -0300

    feat: ajuste de comflitos

--- praticas/pratica04/leia_decimais.c
+++ praticas/pratica04/leia_decimais.c
@@ -9,6 +9,7 @@ int main(){
     printf("Bem vindo ao Sistema de Notas!\n==============================\nDigite a nota com ate duas \ncasas decimamis.\n==============================\n");
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
+    
     printf("Digite a segunda nota: ");
     scanf("%f", &nota2);
     media = (nota1 + nota2) / 2;
